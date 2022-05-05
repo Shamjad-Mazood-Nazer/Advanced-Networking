a543a7802a47c39da2599f158c89118a13fc84fc
Shamjad-Mazood-Nazer <97999729+Shamjad-Mazood-Nazer@users.noreply.github.com>
2022-05-05 15:55:47 +0530

    Shell Script LAB 1

---
+++ SHELL SCRIPT/05-05-2022/myDetails.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#My data
+echo "Enter your Name : "
+read name
+echo "Enter your Age : "
+read age
+echo "Enter your Course : "
+read course
+echo "Enter your College : "
+read college
+echo""
+echo "Your Name : $name"
+echo "Your Age : $age"
+echo "Your Course : $course"
+echo "Your College : $college"
+
