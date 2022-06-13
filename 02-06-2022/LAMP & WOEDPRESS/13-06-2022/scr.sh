322723acbde0f41982f58b448c49bbfbfb480151
Shamjad-Mazood-Nazer <97999729+Shamjad-Mazood-Nazer@users.noreply.github.com>
2022-06-13 16:30:46 +0530

    Apache, LAMP, Wordpress

---
+++ 02-06-2022/LAMP & WOEDPRESS/13-06-2022/scr.sh
@@ -0,0 +1,22 @@
+sudo apt-get remove –purge php*
+sudo apt-get purge php*
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get remove dbconfig-php
+sudo apt-get dist-upgrade
+sudo service apache2 stop
+sudo apt-get purge apache2 apache2-utils apache2.2-bin apache2-common
+sudo apt-get autoremove
+sudo apt-get remove –purge mysql*
+sudo apt-get purge mysql*
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get remove dbconfig-php
+sudo apt-get dist-upgrade
+sudo apt-get remove –purge phpmyadmin*
+sudo apt-get purge phpmyadmin*
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get dist-upgrade
+sudo rm -rf /usr/lib/php
+sudo rm -rf /etc/php /usr/share/php
